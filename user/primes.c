704394af985a220300d69ff1404e44c1ff9be005
Bi Ying <2130043134@qq.com>
2024-04-20 17:59:36 +0800

    lab1 结束！

--- user/primes.c
+++ user/primes.c
@@ -1,62 +1,58 @@
-#include"kernel/types.h"
-#include"user/user.h"
+#include "kernel/types.h"
+#include "user/user.h"
 
-void
-prime(int num, int x){
+void prime(int x) {
+    int num = 0;
+    int len = read(x, &num, sizeof(num));
+    //printf("len %d\n", len);
+    if (len <= 0) {
+        close(x);
+        return;
+    }
     int p[2];
     pipe(p);
     int pid = fork();
-    if(pid > 0){
+
+    if (pid > 0) { // parent process
+        printf("prime %d\n", num);
         int newNum = 0;
-        int len = read(x,&newNum,sizeof(newNum));
-        //printf("prime %d\n",newNum);
-        while(len > 0){
-            if(newNum % num != 0){
-                //printf("newNum : %d\n",newNum);
-                write(p[1],&newNum,sizeof(newNum));
-            }
-            newNum = 0;
-            len = read(x,&newNum,sizeof(newNum));
-        }
         close(p[0]);
+        while (read(x, &newNum, sizeof(newNum)) > 0) {
+            if (newNum % num != 0)
+                write(p[1], &newNum, sizeof(newNum));
+        }
         close(p[1]);
         wait(0);
         exit(0);
-    }else if(pid == 0){
-        int num = 0;
-        read(p[0],&num,sizeof(num));
-        printf("prime %d\n",num);
-        prime(num,p[0]);
+    } else if (pid == 0) { // child process
         close(p[1]);
+        prime(p[0]);
         close(p[0]);
         exit(0);
-    }else{
+    } else {
         printf("fork error.\n");
         exit(1);
     }
 }
-int 
-main(int argc, char *argv[]){
+
+int main(int argc, char *argv[]) {
     int p[2];
     pipe(p);
     int pid = fork();
-    if(pid == 0){
-        int num = 0;
+
+    if (pid == 0) { // child process
         close(p[1]);
-        read(p[0],&num,sizeof(num));
-        printf("prime %d\n",num);
-        prime(num,p[0]);
+        prime(p[0]);
         close(p[0]);
         exit(0);
-    }else if(pid > 0){
+    } else if (pid > 0) { // parent process
         close(p[0]);
-        for(int i = 2; i <= 35; i++){
-            write(p[1],&i,sizeof(i));
-        }
+        for (int i = 2; i <= 35; i++)
+            write(p[1], &i, sizeof(i));
         close(p[1]);
         wait(0);
         exit(0);
-    }else{
+    } else {
         printf("fork error.\n");
         exit(1);
     }
